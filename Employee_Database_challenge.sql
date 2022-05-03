d3e20ca3477944628c59a3a71fdc0d65e7765f48
Semika Burnette <semikaburnette@gmail.com>
2022-05-03 18:54:28 -0400

    %

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,31 @@
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO ret_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_employees_titles
+FROM ret_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no ASC;
+
+
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_employees_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
