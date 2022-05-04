82f9da81926a62493789c4c1e3c5474b006574d5
Semika Burnette <semikaburnette@gmail.com>
2022-05-03 21:01:51 -0400

    %

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+-- Deliverable 1
+
 SELECT e.emp_no,
 e.first_name,
 e.last_name,
@@ -29,3 +31,21 @@ INTO retiring_titles
 FROM unique_employees_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
